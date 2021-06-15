8650581c1b33ddf24b76285b889f6946ab1cd104
thereaper90917 <viper90917@gmail.com>
2021-06-15 16:11:19 +0200

    Fixed security Dependencies added Window theme to about page

--- src/Menu/Menu_JS/about.js
+++ src/Menu/Menu_JS/about.js
@@ -1,55 +1,53 @@
-const { app, BrowserWindow,} = require('electron');
-const path = require('path');
-const appMetaData = require('../../../package.json');
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const appMetaData = require("../../../package.json");
 
-let aboutWindow =  null;
+let aboutWindow = null;
 
 const newWindow = async () => {
-    // if (aboutWindow) {
-    //     aboutWindow.focus()
-    //     return
-    //   }
-  
+  // if (aboutWindow) {
+  //     aboutWindow.focus()
+  //     return
+  //   }
+
   // Create the browser window.
   aboutWindow = new BrowserWindow({
     width: 550,
     height: 450,
     title: "About",
-    resizable:false,
-    vibrancy: 'ultra-dark',
-    autoHideMenuBar: true ,
+    resizable: false,
+    frame: false,
+    vibrancy: "ultra-dark",
+    autoHideMenuBar: true,
     webPreferences: {
-        nodeIntegration: true,
-        contextIsolation: false,
-        devTools: false
-        
-      }
+      preload: path.join(__dirname, "../../preload.js"),
+      enableRemoteModule: true,
+      nodeIntegration: true,
+      contextIsolation: false,
+      devTools: false,
+    },
   });
   // and load the index.html of the app.
-  aboutWindow.loadURL(path.join(__dirname,'../Menu_Html/about.html'))
-
+  aboutWindow.loadURL(path.join(__dirname, "../Menu_Html/about.html"));
   // Sending Version from Package.json to about.html
-  aboutWindow.webContents.send('version', appMetaData.version)
+  aboutWindow.webContents.send("version", appMetaData.version);
 
   // Fix For when you play music and try to close
-  aboutWindow.on('close', function(e) { 
+  aboutWindow.on("close", function (e) {
     e.preventDefault();
     aboutWindow.destroy();
-    });
+  });
   // Open the DevTools.
   // aboutWindow.webContents.openDevTools();
 };
 
-
-
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-
-exports.newWindow = newWindow
+exports.newWindow = newWindow;
